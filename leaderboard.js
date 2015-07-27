0be612c43573dac8a11591410cc86263f879a5d3
magilrojr <magill.jr@gmail.com>
2015-07-27 01:14:27 -0400

    methods chapter, set limit

--- leaderboard.js
+++ leaderboard.js
@@ -29,13 +29,29 @@ if(Meteor.isClient){
             return PlayersList.findOne(selectedPlayer)
         },
     });
-
     Template.leaderboard.events({
         'click .player': function(){
             var playerId = this._id;
             Session.set('selectedPlayer', playerId);
         },
-        'mouseover .increment': function(){
+        'click .increment': function(){
+            var give = 5;
+            var selectedPlayer = Session.get('selectedPlayer');
+            //****this code sets limit for score*****
+            var score = PlayersList.findOne(selectedPlayer).score;
+            score = parseInt(score);
+            Session.set('playerCurScore', score);
+            var playerCurScore = Session.get('playerCurScore')
+            if ( playerCurScore >= 50 ) {
+                give = 0;
+            }
+            else {
+                give = 5;
+            }
+            Meteor.call('modifyPlayerScore', selectedPlayer, give, playerCurScore);
+        },
+        /*'mouseover .increment': function(){
+                 mouseover experiment
                 si = setInterval(function(){
                 var selectedPlayer = Session.get('selectedPlayer');
                 PlayersList.update(selectedPlayer, {$inc: {score: 5}})}, 100);
@@ -44,24 +60,25 @@ if(Meteor.isClient){
         'mouseleave .increment': function(){
             console.log("left increment");
             clearInterval(si);
+                */
         //        si = "";
 
         /*        'blur .increment': function(){
                     console.log("not over increment");
                     clearInterval(si);
-        */
         },
+        */
         'click .decrement': function(){
             var selectedPlayer = Session.get('selectedPlayer');
-            PlayersList.update(selectedPlayer, {$inc: {score: -500}});
-            console.log(selectedPlayer);
+            Meteor.call('modifyPlayerScore', selectedPlayer, -5);
         },
         'click .remove': function(){
             var selectedPlayer = Session.get('selectedPlayer');
-            var confirmPlayer = PlayersList.findOne(selectedPlayer).name;
-            console.log(confirmPlayer);
-            if(confirm("Are you positive you want to remove "+confirmPlayer+"?"))
-            PlayersList.remove(selectedPlayer);
+            Meteor.call('removePlayerData', selectedPlayer);
+            //var confirmPlayer = PlayersList.findOne(selectedPlayer).name;
+            //console.log(confirmPlayer);
+            //if(confirm("Are you positive you want to remove "+confirmPlayer+"?"))
+            //PlayersList.remove(selectedPlayer);
         }
 
         //    newPlayer = prompt("what is your name?", "Rob");
@@ -78,13 +95,9 @@ if(Meteor.isClient){
             var playerNameVar = event.target.playerName.value;
             var playerScoreVar = event.target.quantity.value;
             var parsedScore = parseInt(playerScoreVar);
-            var currentUserId = Meteor.userId();
-            PlayersList.insert({
-                name: playerNameVar,
-                score: parsedScore, createdBy: currentUserId
-            });
-            event.target.playerName.value = null;
-            event.target.quantity.value = null;
+            Meteor.call('insertPlayerData', playerNameVar, parsedScore);
+            // event.target.playerName.value = null;
+            // event.target.quantity.value = null;
         }
     });
     Meteor.subscribe('thePlayers');
@@ -95,5 +108,36 @@ if(Meteor.isServer){
     Meteor.publish('thePlayers', function(){
         var currentUserId = this.userId;
         return PlayersList.find({createdBy: currentUserId})
+    });
+
+    Meteor.methods({
+        /* this gets the users IP Address
+        'sendIpAddress': function(){
+            clientIP = this.connection.clientAddress;
+            console.log(clientIP);
+        }
+        */
+
+        'insertPlayerData': function(playerNameVar, parsedScore){
+            var currentUserId = Meteor.userId();
+            PlayersList.insert({
+                name: playerNameVar,
+                score: parsedScore,
+                createdBy: currentUserId
+            });
+        },
+
+        'removePlayerData': function(selectedPlayer){
+            PlayersList.remove({_id: selectedPlayer, createdBy: currentUserId});
+
+        },
+
+        'modifyPlayerScore': function(selectedPlayer, scoreValue, playerCurScore){
+            var currentUserId = Meteor.userId();
+            PlayersList.update( {_id: selectedPlayer, createdBy: currentUserId},
+                                {$inc: {score: scoreValue} });
+
+
+        }
     });
 }
